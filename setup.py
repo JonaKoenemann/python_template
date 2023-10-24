2925cd04dfd2859ee39a273d7736dc8ba3b8b7f0
jona2708 <jona.koenemann@t-online.de>
2023-10-24 18:27:53 +0200

    ICM

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repository contains a simple python project template.',
+    author='Jona Koenemann',
+    license='',
+)
