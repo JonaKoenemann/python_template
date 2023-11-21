43b4f0225277a63c0e80edd518e1c8412b4a7215
jona2708 <jona.koenemann@t-online.de>
2023-11-21 20:47:35 +0100

    fixed incorrect cookiecutter virable call

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -2,8 +2,8 @@ import re
 import sys
 
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
-module_name = '{{ cookiecutter.module_name }}'
+project_slug = '{{ cookiecutter.project_slug }}'
 
-if not re.match(MODULE_REGEX, module_name):
-    print(f'ERROR: {module_name} is not a valid Python module name!')
+if not re.match(MODULE_REGEX, project_slug):
+    print(f'ERROR: {project_slug} is not a valid project_slug name!')
     sys.exit(1)
