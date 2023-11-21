4f58bd0d84ded1c1a8bac78ef6e041dfddc2ec9a
jona2708 <jona.koenemann@t-online.de>
2023-11-21 20:40:27 +0100

    new method to delete unused folder post gen

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,13 +1,33 @@
+# https://github.com/cookiecutter/cookiecutter/issues/723
+# https://github.com/audreyfeldroy/cookiecutter-pypackage/blob/master/hooks/post_gen_project.py#L4
+
 import os
+import shutil
+import yaml
 
-# REMOVE_PATHS = [
-# '{% if cookiecutter.packaging != "pip" %}requirements.txt{% endif %}',
-# '{% if cookiecutter.packaging != "poetry" %}poetry.lock{% endif %}',
-# ]
+PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+MANIFEST = r"hooks\manifest.yaml"
 
-REMOVE_PATHS = [] # TODO add paths to remove
+def delete_resources_for_disabled_features():
+    with open(MANIFEST) as manifest_file:
+        manifest = yaml.load(manifest_file, yaml.SafeLoader)
+        for feature in manifest['features']:
+            if not feature['enabled']:
+                print(f"removing resources for disabled feature {feature['name']}")
+                for resource in feature['resources']:
+                    delete_resource(resource)
+    print("cleanup complete, removing manifest...")
+    # delete_resource(MANIFEST)
 
-for path in REMOVE_PATHS:
-    path = path.strip()
-    if path and os.path.exists(path):
-        os.unlink(path) if os.path.isfile(path) else os.rmdir(path)
+
+def delete_resource(resource_name):
+    resource = os.path.join(PROJECT_DIRECTORY, resource_name)
+    if os.path.isfile(resource):
+        print(f"removing file: {resource}")
+        os.remove(resource)
+    elif os.path.isdir(resource):
+        print(f"removing directory: {resource}")
+        shutil.rmtree(resource)
+
+if __name__ == "__main__":
+    delete_resources_for_disabled_features()
