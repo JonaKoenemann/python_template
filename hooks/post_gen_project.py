18c0f5521fac85f383eac2646334a5215af9e237
jona2708 <jona.koenemann@t-online.de>
2023-12-07 18:07:05 +0100

    DOC:REF added debug print

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,13 +10,14 @@ import yaml
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 MANIFEST = os.path.join(PROJECT_DIRECTORY, "manifest.yaml")
 
+
 def delete_resources_for_disabled_features():
     with open(MANIFEST) as manifest_file:
         manifest = yaml.load(manifest_file, yaml.SafeLoader)
-        for feature in manifest['features']:
-            if feature['enabled'] == 'false':
+        for feature in manifest["features"]:
+            if feature["enabled"] == "false":
                 print(f"removing resources for disabled feature {feature['name']}")
-                for resource in feature['resources']:
+                for resource in feature["resources"]:
                     delete_resource(resource)
     print("cleanup complete, removing manifest...")
     delete_resource(MANIFEST)
@@ -31,34 +32,51 @@ def delete_resource(resource_name):
         print(f"removing directory: {resource}")
         shutil.rmtree(resource)
 
+
 def init_repo():
     try:
-        subprocess.run(['git', 'init'], check=True)
-        subprocess.run(['git', 'add', '-A'], check=True)
-        subprocess.run(['git', 'commit', '-m', 'initial commit'], check=True)
-        subprocess.run(['git', 'branch', '-M', '{{ cookiecutter.branch_name }}'], check=True)
-        subprocess.run(['git', 'remote', 'add', 'origin', '{{ cookiecutter.repo_url }}'], check=True)
-        subprocess.run(['git', 'push', '-u', 'origin', '{{ cookiecutter.branch_name }}'], check=True)
+        subprocess.run(["git", "init"], check=True)
+        subprocess.run(["git", "add", "-A"], check=True)
+        subprocess.run(["git", "commit", "-m", "initial commit"], check=True)
+        subprocess.run(
+            ["git", "branch", "-M", "{{ cookiecutter.branch_name }}"], check=True
+        )
+        subprocess.run(
+            ["git", "remote", "add", "origin", "{{ cookiecutter.repo_url }}"],
+            check=True,
+        )
+        subprocess.run(
+            ["git", "push", "-u", "origin", "{{ cookiecutter.branch_name }}"],
+            check=True,
+        )
     except:
         raise RuntimeError("Could not init and push repo.")
-    
+
+
 def setup_pre_commits():
     print("Setting up pre-commits.")
     try:
-        subprocess.check_call([sys.executable, '-m', 'pip', '--version']) # check if pip is installed
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "--version"]
+        )  # check if pip is installed
     except:
         raise RuntimeError("Pip is not installed.")
     try:
-        subprocess.run([sys.executable, '-m', 'pip', 'install', 'pre-commit'], check=True)
-        subprocess.run(['pre-commit', 'install', '--install-hooks'], check=True)
+        subprocess.run(
+            [sys.executable, "-m", "pip", "install", "pre-commit"], check=True
+        )
+        subprocess.run(["pre-commit", "install", "--install-hooks"], check=True)
     except:
         raise RuntimeError("Could not install pre-commit hooks.")
 
+
 if __name__ == "__main__":
+    use_pre_commits = "{{ cookiecutter.use_pre_commits }}"
+    print(use_pre_commits, type(use_pre_commits))
     delete_resources_for_disabled_features()
     init_repo()
-    if '{{ cookiecutter.use_pre_commits }}' == 'true':
+    if "{{ cookiecutter.use_pre_commits }}" == "true":
         setup_pre_commits()
     # create .env file
-    file = open('.env', 'w')
-    file.close()   
+    file = open(".env", "w")
+    file.close()
